2db4322501e82b7275b23b4cefa1b646169892ad
gustavodsilva <gustavo.gsilva83@gmail.com>
2026-07-13 18:24:18 -0300

    Adiciona autenticação JWT e configurações iniciais
    Inclui dependências e configuração de autenticação JWT no projeto, adiciona parâmetros de JWT no appsettings.json e cria o serviço TokenService (ainda sem implementação).

--- Marketplace.API/Program.cs
+++ Marketplace.API/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Marketplace.API.Interfaces;
 using Marketplace.API.Repositories;
 using Marketplace.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
 
 var app = builder.Build();
 
---
+++ Marketplace.API/Services/TokenService.cs
@@ -0,0 +1,11 @@
+﻿namespace Marketplace.API.Services;
+
+public class TokenService
+{
+    private readonly IConfiguration _configuration;
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+}
